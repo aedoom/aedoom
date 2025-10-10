2e3ad9ec5b58c958c79dc26e5ede0d2559a50c6b
Andrew Snodgrass <pointlander@gmail.com>
2025-10-10 14:24:15 -0500

    Parallelize

--- pagerank.go
+++ pagerank.go
@@ -28,9 +28,8 @@ type PageRank struct {
 	W, H      int
 	Action    TypeAction
 	Votes     [ActionCount]uint64
-	Started   bool
 	Iteration int
-	Loop      []chan bool
+	Loop      []chan Frame
 }
 
 // NewPageRank new pagerank model
@@ -41,28 +40,53 @@ func NewPageRank() *PageRank {
 	return p
 }
 
-func (p *PageRank) process(loop chan bool, seed int64, index int) {
+func (p *PageRank) process(loop chan Frame, seed int64, index int, x, y int) {
 	rng := rand.New(rand.NewSource(seed))
 	current := 0
 	var ranks [Ranks]uint64
-	for range loop {
-		for range 8 * 256 {
-			sum := uint64(0)
-			for _, value := range p.Markov[index][current] {
-				sum += value
-			}
-			if sum == 0 {
-				current = rng.Intn(Ranks)
-				continue
-			}
-			total, selected := uint64(0), uint64(rng.Intn(int(sum)))
-			for i, value := range p.Markov[index][current] {
-				total += value
-				if selected < total {
-					ranks[i]++
-					current = i
-					break
+	for {
+		for img := range loop {
+			previous := img.GrayAt(x, y).Y
+			for yy := 0; yy < 8; yy++ {
+				for xx := 0; xx < 8; xx++ {
+					current := byte(0)
+					if y&1 == 0 {
+						current = img.GrayAt(x+xx, y+yy).Y
+					} else {
+						current = img.GrayAt(7-x+xx, y+yy).Y
+					}
+					p.Markov[index][previous][current]++
+					p.Markov[index][256+p.Action][current]++
+					for i := range ActionCount {
+						p.Markov[index][current][Pixels+i]++
+					}
+					p.Markov[index][current][Pixels+p.Action]++
+					p.Markov[index][current][Acts+p.Iteration%Cycles]++
+					p.Markov[index][Acts+p.Iteration%Cycles][current]++
+					previous = current
 				}
+			}
+			for range 64 {
+				sum := uint64(0)
+				for _, value := range p.Markov[index][current] {
+					sum += value
+				}
+				if sum == 0 {
+					current = rng.Intn(Ranks)
+					continue
+				}
+				total, selected := uint64(0), uint64(rng.Intn(int(sum)))
+				for i, value := range p.Markov[index][current] {
+					total += value
+					if selected < total {
+						ranks[i]++
+						current = i
+						break
+					}
+				}
+			}
+			if p.Iteration >= Cycles {
+				break
 			}
 		}
 		r := ranks[Pixels:Acts]
@@ -105,47 +129,25 @@ func (p *PageRank) Process(img Frame) (bool, TypeAction) {
 		w, h := width/8, height/8
 		fmt.Println(width, height, w, h, w*h)
 		p.Markov = make([][Ranks][Ranks]uint64, w*h)
-		p.Loop = make([]chan bool, w*h)
+		p.Loop = make([]chan Frame, w*h)
+		fmt.Println(w * h)
 		for i := range p.Loop {
-			p.Loop[i] = make(chan bool, 8)
+			p.Loop[i] = make(chan Frame, 8)
 		}
 		p.W, p.H = w, h
-	}
 
-	index, previous := 0, img.GrayAt(0, 0).Y
-	for y := 0; y < height-8; y += 8 {
-		for x := 0; x < width-8; x += 8 {
-			for yy := 0; yy < 8; yy++ {
-				for xx := 0; xx < 8; xx++ {
-					current := byte(0)
-					if y&1 == 0 {
-						current = img.GrayAt(x+xx, y+yy).Y
-					} else {
-						current = img.GrayAt(7-x+xx, y+yy).Y
-					}
-					p.Markov[index][previous][current]++
-					p.Markov[index][256+p.Action][current]++
-					for i := range ActionCount {
-						p.Markov[index][current][Pixels+i]++
-					}
-					p.Markov[index][current][Pixels+p.Action]++
-					p.Markov[index][current][Acts+p.Iteration%Cycles]++
-					p.Markov[index][Acts+p.Iteration%Cycles][current]++
-					previous = current
-				}
+		index := 0
+		for y := 0; y < height; y += 8 {
+			for x := 0; x < width; x += 8 {
+				go p.process(p.Loop[index], p.Rng.Int63(), index, x, y)
+				index++
 			}
-			index++
 		}
+		fmt.Println(index)
 	}
 
-	if !p.Started {
-		for i := range p.W * p.H {
-			go p.process(p.Loop[i], p.Rng.Int63(), i)
-		}
-		p.Started = true
-		for i := range p.Loop {
-			p.Loop[i] <- true
-		}
+	for i := range p.Loop {
+		p.Loop[i] <- img
 	}
 
 	p.Iteration++
@@ -185,9 +187,6 @@ func (p *PageRank) Process(img Frame) (bool, TypeAction) {
 					}
 				}
 			}
-		}
-		for i := range p.Loop {
-			p.Loop[i] <- true
 		}
 		p.Iteration = 0
 		return true, p.Action
